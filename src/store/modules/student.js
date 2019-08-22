77fa7004a9929d3fb6ab52cab1e9c08411cd01f8
Ahmed Yousef <ahmedyosuefarnous@gmail.com>
2019-08-22 18:15:42 +0200

    Vuex initial Setup

---
+++ src/store/modules/student.js
@@ -0,0 +1,24 @@
+const state = {
+    hello: 'text'
+};
+
+const getters = {
+
+};
+
+const mutations = {
+  
+
+};
+
+
+const actions = {
+    
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,20 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+import student from './modules/student'
+
+import * as actions from './actions'
+import * as mutations from './mutations'
+import * as getters from './getters'
 
 export const store = new Vuex.Store({
   state: {
-    test: "hello"
-  }  
+    host: "http://localhost:8080"
+  },
+  getters,
+  mutations,
+  actions,
+  modules: {
+    student
+  }
 });
